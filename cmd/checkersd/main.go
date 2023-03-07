083056efc0f46168865be346f472f577fb88eb91
Developer Experience team at Ignite <hello@ignite.com>
2023-03-07 17:38:47 +0100

    Initialized with Ignite CLI

---
+++ cmd/checkersd/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"os"
+
+	"github.com/cosmos/cosmos-sdk/server"
+	svrcmd "github.com/cosmos/cosmos-sdk/server/cmd"
+
+	"github.com/bekauz/checkers/app"
+	"github.com/bekauz/checkers/cmd/checkersd/cmd"
+)
+
+func main() {
+	rootCmd, _ := cmd.NewRootCmd()
+	if err := svrcmd.Execute(rootCmd, "", app.DefaultNodeHome); err != nil {
+		switch e := err.(type) {
+		case server.ErrorCode:
+			os.Exit(e.Code)
+
+		default:
+			os.Exit(1)
+		}
+	}
+}
