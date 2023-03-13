0603dc745be979ed7c9e62e56fd20cf2b185de14
bekauz <bekauz@protonmail.com>
2023-03-13 19:12:47 +0100

    further tests

--- x/checkers/types/genesis_test.go
+++ x/checkers/types/genesis_test.go
@@ -7,6 +7,9 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+// three test cases. each takes a made-up genesis object,
+// expected validity result, and some text for the header
+// to indicate what's happening for the reader
 func TestGenesisState_Validate(t *testing.T) {
 	for _, tc := range []struct {
 		desc     string
@@ -53,6 +56,7 @@ func TestGenesisState_Validate(t *testing.T) {
 		},
 		// this line is used by starport scaffolding # types/genesis/testcase
 	} {
+		// each of the tests described above are ran through this runner
 		t.Run(tc.desc, func(t *testing.T) {
 			err := tc.genState.Validate()
 			if tc.valid {
@@ -64,6 +68,7 @@ func TestGenesisState_Validate(t *testing.T) {
 	}
 }
 
+// asserts that default genesis is created as expected
 func TestDefaultGenesisState_ExpectedInitialNextId(t *testing.T) {
 	require.EqualValues(t,
 		&types.GenesisState{
