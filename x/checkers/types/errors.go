e9b66ceecb2183e4d5bba62fa7e6478839d1e202
bekauz <bekauz@protonmail.com>
2023-04-20 13:47:05 +0200

    play move msg; new errors

--- x/checkers/types/errors.go
+++ x/checkers/types/errors.go
@@ -8,7 +8,10 @@ import (
 
 // x/checkers module sentinel errors
 var (
-	ErrInvalidBlack     = sdkerrors.Register(ModuleName, 1100, "black address is invalid: %s")
-	ErrInvalidRed       = sdkerrors.Register(ModuleName, 1101, "red address is invalid: %s")
-	ErrGameNotParseable = sdkerrors.Register(ModuleName, 1102, "game is not parseable")
+	ErrInvalidBlack         = sdkerrors.Register(ModuleName, 1100, "black address is invalid: %s")
+	ErrInvalidRed           = sdkerrors.Register(ModuleName, 1101, "red address is invalid: %s")
+	ErrGameNotParseable     = sdkerrors.Register(ModuleName, 1102, "game is not parseable")
+	ErrInvalidGameIndex     = sdkerrors.Register(ModuleName, 1103, "game index is invalid")
+	ErrInvalidPositionIndex = sdkerrors.Register(ModuleName, 1104, "position index is invalid")
+	ErrMoveAbsent           = sdkerrors.Register(ModuleName, 1105, "move is absent")
 )
